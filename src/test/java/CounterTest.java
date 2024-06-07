1783aa33ca7e5d94bbceb97b3baa41d5784f40f6
Ephymeral Aduare <109929578+rwudboy@users.noreply.github.com>
2024-06-07 09:31:03 +0700

    Update CounterTest.java

--- src/test/java/CounterTest.java
+++ src/test/java/CounterTest.java
@@ -1,23 +1,31 @@
-package src.test.java;
-
-import static org.junit.Assert.*;
-import src.main.java.*;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 public class CounterTest {
 
-	@Test
-	public void testReset() {
-		
-	
-	}
+    @Test
+    public void testReset() {
+        Counter testCounter = new Counter();
+        testCounter.increment();
+        testCounter.reset();
+        // Expected value should be 0 after reset, but we check for a wrong value
+        assertEquals(1, testCounter.getValue(), "Counter reset failed"); // This will fail
+    }
 
-	@Test
-	public void testIncrement() {
-		Counter testCounter = new Counter();
-		
-		
+    @Test
+    public void testIncrement() {
+        Counter testCounter = new Counter();
+        testCounter.increment();
+        // Expected value should be 1 after increment, but we check for a wrong value
+        assertEquals(2, testCounter.getValue(), "Counter increment failed"); // This will fail
+    }
 
-	@Test
-	public void testDecrement() {
-		
+    @Test
+    public void testDecrement() {
+        Counter testCounter = new Counter();
+        testCounter.increment();
+        testCounter.decrement();
+        // Expected value should be 0 after decrement, but we check for a wrong value
+        assertEquals(-1, testCounter.getValue(), "Counter decrement failed"); // This will fail
+    }
+}
